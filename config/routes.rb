5530c272456547386a310eba31786aa8caa74cda
Ubuntu <ubuntu@ip-172-31-41-38.ec2.internal>
2021-03-03 10:35:59 +0000

    Added About, Contact and Help pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/topRated', to: 'static_pages#topRated'
   get '/newArrivals', to: 'static_pages#newArrivals'
   get '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
 end
