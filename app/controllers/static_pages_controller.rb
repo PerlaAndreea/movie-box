97c42f8993fdfc6393dde7666838cea3279974d7
Ubuntu <ubuntu@ip-172-31-41-38.ec2.internal>
2021-03-03 10:50:42 +0000

    Created signup page for new users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,4 +16,7 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
+  
+  def add
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#trending'
   get '/topRated', to: 'static_pages#topRated'
   get '/newArrivals', to: 'static_pages#newArrivals'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
+  get '/add', to: "static_pages#add"
   get '/signup', to: 'users#new'
 end
