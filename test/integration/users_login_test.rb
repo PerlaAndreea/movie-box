24d0f7b98b08e90a9ea04e9f0258da52446f5d2e
Ubuntu <ubuntu@ip-172-31-41-38.ec2.internal>
2021-03-17 17:32:08 +0000

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,12 +15,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
   end
   
-  test "login with remembering" do
+   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-     assert_equal FILL_IN, assigns(:user).FILL_IN
+    assert_not_nil cookies['remember_token']
   end
   
-   test "login without remembering" do
+  test "login without remembering" do
     # Log in to set the cookie.
     log_in_as(@user, remember_me: '1')
     # Log in again and verify that the cookie is deleted.
